2dcea8f422acf76945d4b5b91a9f18e85db4b80e
dmn800 <nefediev.avid@yandex.ru>
2022-10-05 21:48:09 +0500

    3.1. Массив с пустыми ячейками [#176470 #348753]

--- src/main/java/ru/job4j/pojo/Shop.java
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -2,13 +2,11 @@ package ru.job4j.pojo;
 
 public class Shop {
     public static int indexOfNull(Product[] products) {
-        int count = 0;
+        int count = -1;
         for (int i = 0; i < products.length; i++) {
             if (products[i] == null) {
                 count = i;
                 break;
-            } else {
-                count = -1;
             }
         }
         return count;
