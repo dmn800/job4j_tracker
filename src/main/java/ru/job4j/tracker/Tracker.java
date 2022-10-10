415d56966bb7fd1ea48175e7116cf0b35e9e3399
dmn800 <nefediev.avid@yandex.ru>
2022-10-10 22:34:41 +0500

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,9 +1,13 @@
 package ru.job4j.tracker;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
+
+    private String name = null;
 
     public Item add(Item item) {
         item.setId(ids++);
@@ -22,4 +26,29 @@ public class Tracker {
         }
         return rsl;
     }
+
+    public Item[] findAll() {
+        Item[] rsl = new Item[items.length];
+        int size = 0;
+        for (int index = 0; index < items.length; index++) {
+            if (items[index] != null) {
+                rsl[size] = items[index];
+                size++;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[this.size];
+        int size = 0;
+        for (int index = 0; index < this.size; index++) {
+            if (items[index].getName().equals(key)) {
+                rsl[size] = items[index];
+                size++;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+
 }
