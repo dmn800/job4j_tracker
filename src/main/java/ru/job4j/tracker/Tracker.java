040aa4629833b0b104efae8fd1d3a6954a35676e
dmn800 <nefediev.avid@yandex.ru>
2022-10-12 08:55:58 +0500

    6. Метод замены заявки. Tracker.replace [#211748 #350869]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -46,8 +46,10 @@ public class Tracker {
 
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
-        item.setId(id);
-        items[index] = item;
-        return indexOf(id) != -1;
+        if (index != -1) {
+            item.setId(id);
+            items[index] = item;
+        }
+        return index != -1;
     }
 }
