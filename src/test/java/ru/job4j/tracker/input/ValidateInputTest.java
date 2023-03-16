3ab1a1ea6fbcb0be62d5261bfbae65bed0bef694
dmn800 <nefediev.avid@yandex.ru>
2023-03-17 00:01:17 +0500

    2.1. Тест на ValidateInput [#311887]

---
+++ src/test/java/ru/job4j/tracker/input/ValidateInputTest.java
@@ -0,0 +1,58 @@
+package ru.job4j.tracker.input;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.tracker.intface.Input;
+import ru.job4j.tracker.intface.Output;
+import ru.job4j.tracker.output.StubOutput;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ValidateInputTest {
+
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+    }
+
+    @Test
+    public void whenValidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"0"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(0);
+    }
+
+    @Test
+    public void whenManyValidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"0", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("");
+        assertThat(selected).isEqualTo(0);
+        selected = input.askInt("");
+        assertThat(selected).isEqualTo(1);
+    }
+
+    @Test
+    public void whenInvalidMinusOneInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"-1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(-1);
+    }
+
+}
