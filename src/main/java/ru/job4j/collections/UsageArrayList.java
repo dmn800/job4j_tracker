6b8ba09b284f0720de99b33fef1db81cb8baa649
dmn800 <nefediev.avid@yandex.ru>
2023-04-11 19:22:23 +0500

    2. Коллекции, ArrayList, List, Обобщения [#10094]

---
+++ src/main/java/ru/job4j/collections/UsageArrayList.java
@@ -0,0 +1,14 @@
+package ru.job4j.collections;
+import java.util.ArrayList;
+
+public class UsageArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Petr");
+        names.add("Ivan");
+        names.add("Stepan");
+        for (Object value : names) {
+            System.out.println(value);
+        }
+    }
+}
