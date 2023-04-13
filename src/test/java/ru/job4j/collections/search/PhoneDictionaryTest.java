06ce0f5046c2a4b31bbcdb7c8b166311727cb45a
dmn800 <nefediev.avid@yandex.ru>
2023-04-14 00:03:29 +0500

    4. Телефонный справочник на базе ArrayList [#41598]

---
+++ src/test/java/ru/job4j/collections/search/PhoneDictionaryTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.collections.search;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
+    }
+
+    @Test
+    public void whenIsEmpty() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("5344");
+        assertThat(persons).isEmpty();
+    }
+}
