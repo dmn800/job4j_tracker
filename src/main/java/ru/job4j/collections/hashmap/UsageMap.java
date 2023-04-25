a63024ccc107c81fbf7bcad059b640df9cd1f31d
dmn800 <nefediev.avid@yandex.ru>
2023-04-25 06:16:06 +0500

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collections/hashmap/UsageMap.java
@@ -0,0 +1,18 @@
+package ru.job4j.collections.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> data = new HashMap<>();
+        data.put("job4j@yandex.ru", "Иванов Иван Иванович");
+        data.put("job4j@gmail.ru", "Петров Петр Петрович");
+
+        for (Map.Entry<String, String> entry : data.entrySet()) {
+            String mail = entry.getKey();
+            String fullName = entry.getValue();
+            System.out.println(mail + " = " + fullName);
+        }
+    }
+}
